895e01aebad26881d1a5ec0a282887f865de4c38
olgrea <olgrea@gmail.com>
2016-04-16 12:05:25 -0400

    implemented word index

--- Program.cs
+++ Program.cs
@@ -1,15 +1,73 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
-using System.Threading.Tasks;
+
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
 
 namespace PDFIndex
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            System.IO.FileInfo fi = null;
+            try
+            {
+                fi = new System.IO.FileInfo(args[1]);
+            }
+            catch (ArgumentException)
+            {
+            }
+            catch (System.IO.PathTooLongException)
+            {
+            }
+            catch (NotSupportedException)
+            {
+            }
+            if (ReferenceEquals(fi, null))
+            {
+                Console.WriteLine("Please provide a valid file path.");
+                return;
+            }
+
+            Dictionary<string, List<int>> wordIndex = BuildIndexFromPdf(args[1]);
+
+
+        }
+
+        private static Dictionary<string, List<int>> BuildIndexFromPdf(string path)
+        {
+            var wordIndex = new Dictionary<string, List<int>>();
+
+            using (PdfReader reader = new PdfReader(path))
+            {
+                for (int page = 1; page <= reader.NumberOfPages; page++)
+                {
+                    string pageText = PdfTextExtractor.GetTextFromPage(reader, page);
+
+                    string[] words = pageText.Split(' ');
+
+                    foreach (string word in words.Where(w => w.Length > 2))
+                    {
+                        string w = word.ToLower();
+                        if (!wordIndex.ContainsKey(w))
+                        {
+                            wordIndex[w] = new List<int>();
+                        }
+
+                        if (wordIndex[w].Contains(page))
+                        {
+                            wordIndex[w].Add(page);
+                        }
+                    }
+                }
+            }
+
+            return wordIndex;
         }
     }
 }
