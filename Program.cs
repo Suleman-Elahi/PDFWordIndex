5dd7fa941567387852a5fa02326da3b9cbb6a555
olgrea <olgrea@gmail.com>
2016-04-16 13:09:31 -0400

    Word index is working

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
@@ -17,7 +18,7 @@ namespace PDFIndex
             System.IO.FileInfo fi = null;
             try
             {
-                fi = new System.IO.FileInfo(args[1]);
+                fi = new System.IO.FileInfo(args[0]);
             }
             catch (ArgumentException)
             {
@@ -34,24 +35,61 @@ namespace PDFIndex
                 return;
             }
 
-            Dictionary<string, List<int>> wordIndex = BuildIndexFromPdf(args[1]);
+            string filename = args[0];
+            Console.WriteLine("Building word index for \n{0} ...", filename);
 
+            SortedDictionary<string, List<int>> wordIndex = BuildIndexFromPdf(filename);
 
+            var file = new System.IO.StreamWriter("word_index.txt");
+            foreach (var word in wordIndex)
+            {
+                string values = null;
+                word.Value.ForEach(v => values += v + ", ");
+                file.WriteLine("{0}\t\t{1}", word.Key, values);
+            }
+
+            Console.WriteLine("Completed! nb of words: {0}", wordIndex.Count);
         }
 
-        private static Dictionary<string, List<int>> BuildIndexFromPdf(string path)
+        private static SortedDictionary<string, List<int>> BuildIndexFromPdf(string path)
         {
-            var wordIndex = new Dictionary<string, List<int>>();
+            var wordIndex = new SortedDictionary<string, List<int>>();
 
             using (PdfReader reader = new PdfReader(path))
             {
                 for (int page = 1; page <= reader.NumberOfPages; page++)
                 {
-                    string pageText = PdfTextExtractor.GetTextFromPage(reader, page);
+                    string pageText = null;
+                    try
+                    {
+                        pageText = PdfTextExtractor.GetTextFromPage(reader, page);
+                    }
+                    catch (InlineImageUtils.InlineImageParseException e)
+                    {
+                        Console.WriteLine("Exception! {0}. ", e.Message);
+                        Console.WriteLine("Skipping page {0} ...", page);
+                        continue;
+                    }
 
-                    string[] words = pageText.Split(' ');
+                    var sb = new StringBuilder();
+                    char[] arr = pageText.ToCharArray();
+                    foreach (char c in arr)
+                    {
+                        if ((char.IsLetterOrDigit(c) || char.IsWhiteSpace(c) || c == '-'))
+                        {
+                            sb.Append(c);
+                        }
+                        else
+                        {
+                            sb.Append(' ');
+                        }
+                    }
 
-                    foreach (string word in words.Where(w => w.Length > 2))
+                    pageText = sb.ToString();
+                    string[] words = pageText.Split(new []{' ', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+
+                    int n;
+                    foreach (string word in words.Where(w => w.Length > 2 && !int.TryParse(w, out n)))
                     {
                         string w = word.ToLower();
                         if (!wordIndex.ContainsKey(w))
@@ -59,7 +97,7 @@ namespace PDFIndex
                             wordIndex[w] = new List<int>();
                         }
 
-                        if (wordIndex[w].Contains(page))
+                        if (!wordIndex[w].Contains(page))
                         {
                             wordIndex[w].Add(page);
                         }
